66bc8d9d00eb77956762117e00f2755d5c3be411
Carl Tornberg <carltornberg@hotmail.com>
2026-01-22 12:28:17 +0100

    Removed helper functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,39 +1,23 @@
 #![no_std]
-
+pub mod helpers;
 pub mod interface;
 pub mod states; 
-mod processors;
+pub mod errors;
 pub mod types;
-use core::mem::MaybeUninit;
-
-
+mod processors;
 use pinocchio::{
-  AccountView, Address, ProgramResult, entrypoint, error::ProgramError, hint::unlikely
+    AccountView, 
+    Address, 
+    ProgramResult, 
+    entrypoint, 
+    error::ProgramError, 
+    hint::unlikely
 };
-
-solana_address::declare_id!("GJJuYV5QA1Lt9Ht5rdmVgvXdgjTJDe7nfJQ47YLvdstV");
-
-
-#[inline(always)]
-pub fn write_bytes(destination: &mut [MaybeUninit<u8>], source: &[u8]) {
-
-    // SAFETY:
-    // - Pointers are of alignment 1,
-    // - the length will not exceed either pointers length
-    unsafe {
-        core::ptr::copy_nonoverlapping(
-            source.as_ptr(), 
-            destination.as_mut_ptr() as *mut u8, 
-            destination.len().min(source.len())
-        );
-    }
-}
-
 use solana_address::address_eq;
 use solana_program_log::log;
-
 use crate::interface::ProgramInstructions;
 
+solana_address::declare_id!("GJJuYV5QA1Lt9Ht5rdmVgvXdgjTJDe7nfJQ47YLvdstV");
 
 entrypoint!(process_instruction);
 
@@ -46,7 +30,6 @@ pub fn process_instruction(
         return Err(ProgramError::IncorrectProgramId);
     }
 
-    log("Hello from my pinocchio program!");
     let [discr, instruction_data @ ..] = instruction_data else {
         return Err(pinocchio::error::ProgramError::InvalidInstructionData);
     };
@@ -54,7 +37,7 @@ pub fn process_instruction(
     match (*discr).try_into()? {
         ProgramInstructions::MyInstruction => {
             log!("My instruction.");
-            processors::process_something_processor(instruction_data, accounts)
+            processors::process_my_instruction(instruction_data, accounts)
         },
     }
 }
