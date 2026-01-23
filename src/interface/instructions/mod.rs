7938b68e7ba2f8b3374e6aba3a279da879f959c6
Carl Tornberg <carltornberg@hotmail.com>
2026-01-23 15:36:43 +0100

    Fixed incorrect trait name. Fixed incorrect error used. Changed Into to From

--- src/interface/instructions/mod.rs
+++ src/interface/instructions/mod.rs
@@ -1,11 +1,11 @@
 pub mod my_instruction;
 
-use core::ptr::copy_nonoverlapping;
+use core::{mem::MaybeUninit, ptr::copy_nonoverlapping};
 
 pub use my_instruction::*;
 use pinocchio::error::ProgramError;
 
-use crate::{helpers::bytes_helpers::{Transmutable, as_bytes}, types::Discriminator};
+use crate::{errors::CustomError, helpers::bytes_helpers::Transmutable, types::Discriminator};
 
 #[repr(u8)]
 pub enum ProgramInstructions {
@@ -17,9 +17,9 @@ unsafe impl Transmutable for ProgramInstructions {
     const LEN: usize = size_of::<Self>();
 }
 
-impl Into<Discriminator> for ProgramInstructions {
-    fn into(self) -> Discriminator {
-        self as Discriminator
+impl From<ProgramInstructions> for Discriminator {
+    fn from(value: ProgramInstructions) -> Self {
+        value as Discriminator
     }
 }
 
@@ -37,15 +37,18 @@ impl TryFrom<u8> for ProgramInstructions {
     }
 }
 
-/// Trait used for struct which are an instruction data
-pub trait ProgramInstruction {}
+/// Trait used for struct which are instruction data
+pub trait InstructionData { }
 
 /// Copies the instruction type and data as bytes to the provided buffer slice.
 /// Buf must be of sufficient length.
-pub fn instruction_to_bytes<'a, T>(buf: &mut [u8], inst_type: &'a ProgramInstructions, inst_data: &'a T) -> Result<(), ProgramError>
-where T: Transmutable + ProgramInstruction, {
+pub fn instruction_to_bytes<'a, T>(
+    buf: &mut [MaybeUninit<u8>], 
+    inst_type: &'a ProgramInstructions, 
+    inst_data: &'a T) -> Result<(), ProgramError> 
+where T: Transmutable + InstructionData, {
     if buf.len() < ProgramInstructions::LEN + T::LEN {
-        return Err(ProgramError::BorshIoError); // TODO Change to a better error?
+        return Err(CustomError::TransmutableError.into()); 
     }
 
     unsafe {
@@ -57,11 +60,10 @@ where T: Transmutable + ProgramInstruction, {
 
     unsafe {
         copy_nonoverlapping(
-            as_bytes(inst_data).as_ptr(),
+            inst_data.as_bytes().as_ptr(),
             buf.as_mut_ptr().add(size_of::<ProgramInstructions>()) as _, 
             size_of::<T>());
     }
 
     Ok(())
-
 }
